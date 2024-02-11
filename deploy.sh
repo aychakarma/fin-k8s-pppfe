17f3845f2cc8cea50b10179e21fc9ecd084e418e
aychakarma <karma.aycha@esprit.tn>
2024-02-11 18:43:48 +0100

    te

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,20 @@
-KUBECTL_PATH="/usr/bin/kubectl"
+#!/bin/bash
 
-chmod +x "$KUBECTL_PATH"
+# Définition du chemin vers kubectl
+KUBECTL_PATH=/usr/bin/kubectl
 
+# Changement vers le répertoire contenant les fichiers YAML
 cd kubernetes
 
+# Application des ressources YAML avec kubectl
 ${KUBECTL_PATH} apply -f secret.yaml
 sleep 2
 ${KUBECTL_PATH} apply -f configmap.yaml
 sleep 2
-${KUBECTL_PATH} apply -f mysql-deploy.yml 
+${KUBECTL_PATH} apply -f mysql-deploy.yml
 sleep 2
 ${KUBECTL_PATH} apply -f mysql-service.yml
 sleep 2
-${KUBECTL_PATH} apply -f app-deploy.yml 
+${KUBECTL_PATH} apply -f app-deploy.yml
 sleep 2
 ${KUBECTL_PATH} apply -f app-service.yml
